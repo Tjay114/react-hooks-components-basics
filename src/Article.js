73f1fda19de0488aa110e0bb9a6d13684102a9b0
Terormumuli <terahmumuli@gmail.com>
2022-07-12 22:03:11 +0300

    Components Lab

--- src/Article.js
+++ src/Article.js
@@ -1,9 +1,16 @@
 import React from "react";
 
+
 function Article() {
   return (
     <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
   );
 }
-
+function Comment() {
+  return (
+  <div>Naturally, I agree with this article.</div>
+  );
+}
 export default Article;
+
+
